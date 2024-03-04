0dbc985792930b76de290f236ba2151c5eb35c85
Yarik <yaroslav.matsapura@gmail.com>
2024-03-04 13:48:08 +0200

    feat: add imports

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from config import POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, DateTime
 # SQLAlchemy setup
 DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = create_engine(DATABASE_URL)
@@ -15,3 +17,43 @@ Column("latitude", Float),
 Column("longitude", Float),
 Column("timestamp", DateTime),
 )
+
+
+# # FastAPI models
+# class AccelerometerData(BaseModel):
+#     x: float
+#     y: float
+#     z: float
+
+# class GpsData(BaseModel):
+#     latitude: float
+#     longitude: float
+
+# class AgentData(BaseModel):
+#     accelerometer: AccelerometerData
+#     gps: GpsData
+#     timestamp: datetime
+#     @classmethod
+#     @field_validator('timestamp', mode='before')
+#     def check_timestamp(cls, value):
+#         if isinstance(value, datetime):
+#             return value
+#         try:
+#             return datetime.fromisoformat(value)
+#         except (TypeError, ValueError):
+#             raise ValueError("Invalid timestamp format. Expected ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ).")
+
+# class ProcessedAgentData(BaseModel):
+#     road_state: str
+#     agent_data: AgentData
+
+# # Database model
+# class ProcessedAgentDataInDB(BaseModel):
+#     id: int
+#     road_state: str
+#     x: float
+#     y: float
+#     z: float
+#     latitude: float
+#     longitude: float
+#     timestamp: datetime
